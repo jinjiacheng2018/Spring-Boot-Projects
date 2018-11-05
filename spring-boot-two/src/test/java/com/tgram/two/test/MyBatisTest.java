6f067d35f2918dfbede1ba2daba0ee2695c5e439
jinjiacheng2018 <1935922807@qq.com>
2018-11-05 09:35:45 +0800

    MyBatis连接数据库

--- spring-boot-two/src/test/java/com/tgram/two/test/MyBatisTest.java
+++ spring-boot-two/src/test/java/com/tgram/two/test/MyBatisTest.java
@@ -1,18 +1,18 @@
 package com.tgram.two.test;
 
-import com.tgram.two.dao.StudentDao;
-import com.tgram.two.entity.Student;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
+import com.tgram.two.dao.StudentDao;
+import com.tgram.two.entity.Student;
 
 /**
- *<p> Description: 描述信息 </p>
+ *<p> Description: 测试类信息 </p>
  *<p> Copyright: Copyright(c) 2018/11/4 </p>
  *<p> Company: 7qb </p>
  *
