6b6858612b59202c73d4f965fa753a8d01a1fa3f
jinjiacheng2018 <1935922807@qq.com>
2018-11-05 17:51:30 +0800

    SpringBoot集成MyBatis

--- spring-boot-two/src/test/java/com/tgram/two/SpringBootTwoApplicationTests.java
+++ spring-boot-two/src/test/java/com/tgram/two/SpringBootTwoApplicationTests.java
@@ -1,16 +1,32 @@
 package com.tgram.two;
 
+import com.tgram.two.entity.Student;
+import com.tgram.two.service.StudentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {SpringBootTwoApplication.class})
 public class SpringBootTwoApplicationTests {
+
+	
+
+	@Resource
+	private StudentService studentService;
 
 	@Test
 	public void contextLoads() {
+		List<Student> students = studentService.queryAllStudents();
+
+		for (Student student : students) {
+			System.out.println(student);
+		}
 	}
 
 }
