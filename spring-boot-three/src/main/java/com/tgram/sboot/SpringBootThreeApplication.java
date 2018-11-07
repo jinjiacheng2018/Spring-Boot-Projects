c268f5f38c87dab1ea3a4c78dd582725a32870a5
jinjiacheng2018 <1935922807@qq.com>
2018-11-07 17:47:26 +0800

    添加项目

---
+++ spring-boot-three/src/main/java/com/tgram/sboot/SpringBootThreeApplication.java
@@ -0,0 +1,14 @@
+package com.tgram.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThreeApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootThreeApplication.class, args);
+    }
+}
