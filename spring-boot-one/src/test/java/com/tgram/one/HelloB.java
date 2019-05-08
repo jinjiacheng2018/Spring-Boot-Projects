f477d35e66ac310ef05856cb9b5928dd185d5bd5
jinjiacheng2018 <1935922807@qq.com>
2019-05-08 11:07:22 +0800

    静态代码块、代码块、构造方法

---
+++ spring-boot-one/src/test/java/com/tgram/one/HelloB.java
@@ -0,0 +1,38 @@
+package com.tgram.one;
+
+public class HelloB extends HelloA
+{
+    public HelloB()
+    {
+        System.out.println("I’m B");
+    }
+    
+    {
+        System.out.println("I’m B class");
+    }
+    static
+    {
+        System.out.println("static B");
+    }
+    
+    public static void main(String[] args)
+    {
+        new HelloB();
+    }
+}
+
+class HelloA
+{
+    public HelloA()
+    {
+        System.out.println("I’m A");
+    }
+    
+    {
+        System.out.println("I’m A class");
+    }
+    static
+    {
+        System.out.println("static A");
+    }
+}
